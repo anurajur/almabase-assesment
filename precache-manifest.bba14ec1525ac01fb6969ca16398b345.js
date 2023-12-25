bad77d2188d1ff08296c49979e0914fd8a60d284
anurajur <anuradharajur95@gmail.com>
2023-12-25 17:56:35 +0530

    Updates

---
+++ precache-manifest.bba14ec1525ac01fb6969ca16398b345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d25e46b7791737f1bc8d81fb03fe746",
+    "url": "/almabase-assesment/index.html"
+  },
+  {
+    "revision": "33106053a806ead0baa0",
+    "url": "/almabase-assesment/static/css/main.cbcf82e2.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/almabase-assesment/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/almabase-assesment/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "33106053a806ead0baa0",
+    "url": "/almabase-assesment/static/js/main.b3bfec76.chunk.js"
+  },
+  {
+    "revision": "63cd4d24228dc2f9f091",
+    "url": "/almabase-assesment/static/js/runtime~main.0a38a080.js"
+  }
+]);
