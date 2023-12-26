13b2a0279826d44162208fecf07d62f8d9a18aca
Anuradha Rajur <55488029+anurajur@users.noreply.github.com>
2023-12-26 11:18:39 +0530

    Update DropArea.js

--- src/components/DropArea.js
+++ src/components/DropArea.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//DropArea component represents a draggable area where items can be dropped.
 function DropArea({ onDrop, onDragOver, onClick, children }) {
   return (
     <div
